d1f95488bdbf2e1a7963ac7c007434e1e0226eb5
luisandia <luisandiamamani@gmail.com>
2020-03-20 16:27:19 -0500

    set url

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,7 +1,8 @@
 
 // url
 // export default "https://vintage-tech-store-react-api.herokuapp.com";
+export default "https://react-lsad-store.herokuapp.com";
 
 // local fix
-export default "http://localhost:1337"
+// export default "http://localhost:1337"
 // setup free claudinary
